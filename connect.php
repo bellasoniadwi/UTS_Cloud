07f841ee52a8c00d4aac2ada62282979d2341b78
BELLA <95678334+bellasoniadwi@users.noreply.github.com>
2022-10-26 23:41:56 +0700

    Delete connect.php

--- connect.php
+++
@@ -1,7 +0,0 @@
-<?php
-$connect = mysqli_connect("localhost", "root", "", "my_cloud");
-
-// Check connection
-if (mysqli_connect_errno()) {
-    echo "Connection Failed : " . mysqli_connect_error();
-}
